30aaebca75f54bec3194727158ceb2f99a75c10e
root <valentina.zaccolo@to.infn.it>
2017-02-17 01:02:38 -0500

    structure for ini shares file implemented

--- src/pm/InitShares.cc
+++ src/pm/InitShares.cc
@@ -13,6 +13,12 @@
 #include <iostream>
 #include <sstream>
 
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
+
+
 //#include <unistd.h>
 //#include <sys/stat.h>
 
@@ -21,13 +27,54 @@ using namespace std;
 
 /* -------------------------------------------------------------------------- */
 
+
+template < typename T >
+boost::program_options::typed_value< T >* make_value( T* store_to )
+{
+  return boost::program_options::value< T >( store_to );
+}
+
+struct user 
+{
+  unsigned short userID;
+  unsigned short groudID;
+  unsigned short share;
+
+ user (unsigned short userID,
+       unsigned short groupID, 
+       unsigned short share )
+    : userID(userID),
+      groupID(groupID),
+      share (share)
+  {}
+};
+
+user make_user( const std::string& user_group_share )
+{
+  std::vector< std::string > tokens;
+  boost::split( tokens, user_group_share, boost::is_any_of( ":" ) );
+
+  if ( 3 != tokens.size() )
+  {
+     using boost::program_options::validation_error;
+     throw validation_error(validation_error::invalid_option_value,
+                            "users.user",
+                            user_group_share);
+  }
+  return slave(boost::lexical_cast<unsigned short>(tokens[0]),
+	       boost::lexical_cast<unsigned short>(tokens[1]),
+               boost::lexical_cast<unsigned short>(tokens[2]));
+}
+
 bool InitShares::load_shares(){
+
+    std::vector<std::string> user_configs;
 
     cout << "Loading initial shares..." << endl;
     po::options_description desc("Allowed options");
 
     desc.add_options()
-		("init_shares", po::value<string>()->default_value("0:0:0"), "initial shares")
+		("users.user", make_value(&user_configs), "initial shares")
 		;
 
     /// Read the initial shares file
