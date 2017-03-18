26c5230848139cacc93645cf69371f87bf2fe0bd
Valentina Zaccolo <valentina.zaccolo@to.infn.it>
2017-03-18 16:28:52 +0100

    Update RequestSystem.cc

--- src/rpcm/RequestSystem.cc
+++ src/rpcm/RequestSystem.cc
@@ -19,14 +19,10 @@
 
 void SystemVersion::request_execute(xmlrpc_c::paramList const& paramList,
                                  RequestAttributes& att){
-  
     /** Request_execute will not be executed if the session string
      * is not authenticated in Request::execute.
      * TODO: should we make the version call accessible even
      * if no user is provided? */
-    
     success_response(Fass::instance().version(), att);
-    
     return;
 }
-
