7f51ffdbd5cd02d456480c11ed1ba179beaf9a19
Valentina Zaccolo <valentina.zaccolo@to.infn.it>
2017-03-20 11:18:10 +0100

    Update Log.cc

--- src/logger/Log.cc
+++ src/logger/Log.cc
@@ -16,24 +16,20 @@
 
 #include "Log.h"
 
-//#include <string.h>
-//#include <stdlib.h>
+// #include <string.h>
+// #include <stdlib.h>
 #include <stdexcept>
-//#include <sstream>
-//#include <iostream>
+// #include <sstream>
+// #include <iostream>
 
-//#include <sys/types.h>
-//#include <unistd.h>
-
-using namespace std;
+// #include <unistd.h>
 
 const char Log::error_names[] ={ 'E', 'W', 'I', 'D', 'D', 'D' };
 
 FileLog::FileLog(const string&   file_name,
                  const MessageType   level,
                  ios_base::openmode  mode)
-        :Log(level), log_file_name(file_name)
-{
+        :Log(level), log_file_name(file_name) {
     ofstream file;
 
     file.open(log_file_name.c_str(), mode);
@@ -43,8 +39,7 @@ FileLog::FileLog(const string&   file_name,
         throw runtime_error("Could not open log file");
     }
 
-    if ( file.is_open() == true )
-    {
+    if ( file.is_open() == true ) {
         file.close();
     }
 }
@@ -58,24 +53,21 @@ FileLog::~FileLog() { }
 void FileLog::log(
     const char *            module,
     const MessageType       type,
-    const char *            message)
-{
+    const char *            message) {
     char        str[26];
     time_t      the_time;
     ofstream    file;
 
-    if( type <= log_level)
-    {
+    if (type <= log_level) {
         file.open(log_file_name.c_str(), ios_base::app);
 
-        if (file.fail() == true)
-        {
+        if (file.fail() == true) {
             return;
         }
 
         the_time = time(NULL);
 
-        ctime_r(&(the_time),str);
+        ctime_r(&(the_time), str);
 
         /// Get rid of final enter character
         str[24] = '\0';
